7d60853cc42224183733ba4e6d973bd35faa4912
fuckiebrowser <1608880758@qq.com>
2017-11-16 17:37:57 +0800

    rewrite badge style

--- config/index.js
+++ config/index.js
@@ -14,7 +14,7 @@ module.exports = {
     proxyTable: {},
 
     // Various Dev Server settings
-    host: '10.1.39.111', // can be overwritten by process.env.HOST
+    host: 'localhost', // can be overwritten by process.env.HOST
     port: 5230, // can be overwritten by process.env.HOST, if port is in use, a free one will be determined
     autoOpenBrowser: true,
     errorOverlay: true,
