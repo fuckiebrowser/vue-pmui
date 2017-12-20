6a7b6207d36cd7701a89b1901d78b47436838659
fuckiebrowser <1608880758@qq.com>
2017-12-20 09:24:32 +0800

    add test for Button component

---
+++ test/unit/specs/button.spec.js
@@ -0,0 +1,107 @@
+import { createTest } from '../utils';
+import Button from '../../../src/components/button';
+
+describe('Button', () => {
+  let vm;
+  afterEach(() => {
+    if (vm) {
+      vm.$destroy();
+      vm = null;
+    }
+  });
+
+  it('create', () => {
+    vm = createTest(Button);
+    const dom = vm.$el;
+    expect(dom.classList.contains('gay-button')).to.equal(true);
+    ['primary', 'success', 'danger', 'warning', 'small', 'long', 'round', 'disabled']
+      .forEach(async (cssClass) => {
+        expect(dom.classList.contains(`gay-button__${cssClass}`)).to.equal(false);
+      });
+  });
+
+  ['primary', 'success', 'danger', 'warning'].forEach((type) => {
+    it(`type ${type}`, async () => {
+      vm = createTest(Button, { type });
+      const dom = vm.$el;
+      await vm.$nextTick();
+      expect(dom.classList.contains(`gay-button__${type}`)).to.equal(true);
+    });
+  });
+
+  it('size small', () => {
+    vm = createTest(Button, { size: 'small' });
+    const dom = vm.$el;
+    expect(dom.classList.contains('gay-button__small')).to.equal(true);
+  });
+
+  it('long', () => {
+    vm = createTest(Button, { long: true });
+    const dom = vm.$el;
+    expect(dom.classList.contains('gay-button__long')).to.equal(true);
+  });
+
+  it('round', () => {
+    vm = createTest(Button, { round: true });
+    const dom = vm.$el;
+    expect(dom.classList.contains('gay-button__round')).to.equal(true);
+  });
+
+  it('disabled', () => {
+    vm = createTest(Button, { disabled: true });
+    const dom = vm.$el;
+    expect(dom.classList.contains('gay-button__disabled')).to.equal(true);
+  });
+
+  it('click enable', () => {
+    const clickHandler = sinon.spy();
+    const disabled = false;
+    vm = createTest({
+      data() {
+        return {
+          int: 0
+        };
+      },
+      render(h) {
+        return h(Button, {
+          props: { disabled },
+          on: { click: clickHandler }
+        }, 'button');
+      },
+      methods: {
+        clickHandler() {
+          this.int += 1;
+        }
+      }
+    }, true);
+    expect(clickHandler).to.have.callCount(0);
+    vm.$el.click();
+    expect(clickHandler).to.have.been.callCount(1);
+  });
+
+  it('click disabled', () => {
+    const clickHandler = sinon.spy();
+    const disabled = true;
+    vm = createTest({
+      data() {
+        return {
+          int: 0
+        };
+      },
+      render(h) {
+        return h(Button, {
+          props: { disabled },
+          on: { click: clickHandler }
+        }, 'button');
+      },
+      methods: {
+        clickHandler() {
+          this.int += 1;
+        }
+      }
+    }, true);
+    expect(clickHandler).to.have.callCount(0);
+    vm.$el.click();
+    expect(clickHandler).to.have.been.callCount(0);
+  });
+});
---
+++ test/unit/utils.js
@@ -0,0 +1,45 @@
+import Vue from 'vue';
+import GayUI from '../../src/index';
+
+Vue.use(GayUI);
+
+/**
+ * @var id 自增
+ */
+let id = 0;
+
+/**
+ * @desc 创建元素节点
+ * @return {Object} dom对象
+ */
+function createDom() {
+  // 创建节点
+  const dom = document.createElement('div');
+  // 设置节点ID
+  dom.id = `test-${id += 1}`;
+  // 插入到body节点
+  document.body.appendChild(dom);
+  return dom;
+}
+
+/**
+ * @desc
+ * @param {Object} component Vue组件
+ * @param {Object} propsData Vue组件props参数
+ * @param {Boolean} mount 是否挂载到dom上
+ * @returns {Object} 返回Vue实例
+ */
+export function createTest(component, propsData, mount = false) {
+  // 假如没有第三个参数且第二个参数为Boolean
+  if (propsData === true || propsData === false) {
+    mount = propsData;
+    propsData = {};
+  }
+  // 创建挂载节点
+  const dom = createDom();
+  const Instance = Vue.extend(component);
+  return new Instance({ propsData }).$mount(mount === true ? dom : null);
+}
+
+
+export default null;
